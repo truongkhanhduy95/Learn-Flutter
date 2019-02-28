713fb050bf22d2332c3f591da286708496b8f4a4
Duy Truong <duy.truong@contemi.com.vn>
2019-02-28 18:42:45 +0700

    Update home.dart

--- srine/mdc_100_series/lib/home.dart
+++ srine/mdc_100_series/lib/home.dart
@@ -22,10 +22,55 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        title: Text("SHRINE"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, semanticLabel: 'search'),
+            onPressed: (){ print('search button');},
+          ),
+          IconButton(
+            icon: Icon(Icons.tune, semanticLabel: 'filter'),
+            onPressed: (){print('filter button');}
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        // TODO: Build a grid of cards (102)
+        children: <Widget>[
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png'),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      SizedBox(height: 8.0),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
