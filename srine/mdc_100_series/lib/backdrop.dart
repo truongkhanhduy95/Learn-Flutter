352747eff4c336ff0b982b1a3769caec7c7d7c71
Duy Truong <truongkhanhduy9@gmail.com>
2019-03-01 15:36:51 +0700

    Update animation

--- srine/mdc_100_series/lib/backdrop.dart
+++ srine/mdc_100_series/lib/backdrop.dart
@@ -3,7 +3,8 @@ import 'package:meta/meta.dart';
 
 import 'model/product.dart';
 
-// TODO: Add velocity constant (104)
+const double _kFlingVelocity = 2.0;
+
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
   final Widget frontLayer;
@@ -29,16 +30,65 @@ class Backdrop extends StatefulWidget {
 
 class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin{
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
-  // TODO: Add AnimationController widget (104)
+  AnimationController _controller;
 
-  // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  // TODO: Add override for didUpdateWidget (104)
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  bool get _frontLayerVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed || status ==AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+      velocity: _frontLayerVisible ? - _kFlingVelocity :_kFlingVelocity
+    );
+  }
+
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    // TODO: Create a RelativeRectTween Animation (104)
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
-        widget.backLayer,
-        widget.frontLayer
-      ]
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        // TODO: Add a PositionedTransition (104)
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(
+            // TODO: Implement onTap property on _BackdropState (104)
+            child: widget.frontLayer,
+          ),
+        ),
+      ],
     );
   }
 
@@ -51,7 +101,10 @@ class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin
       // TODO: Replace leading menu icon with IconButton (104)
       // TODO: Remove leading property (104)
       // TODO: Create title with _BackdropTitle parameter (104)
-      leading: Icon(Icons.menu),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: _toggleBackdropLayerVisibility,
+      ),
       title: Text('SHRINE'),
       actions: <Widget>[
         // TODO: Add shortcut to login screen from trailing icons (104)
@@ -77,12 +130,38 @@ class _BackdropState extends State<Backdrop> with SingleTickerProviderStateMixin
     );
     return Scaffold(
       appBar: appBar,
-      // TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+       body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
 
-// TODO: Add _FrontLayer class (104)
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-tap callback (104)
+  const _FrontLayer({
+    Key key,
+    this.child,
+  }) : super (key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
 // TODO: Add _BackdropTitle class (104)
 // TODO: Add _BackdropState class (104)
