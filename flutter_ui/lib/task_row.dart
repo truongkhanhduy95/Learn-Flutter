c5f24ebae13f1d4a8480c0469e398887891b9f71
Duy Truong <truongkhanhduy9@gmail.com>
2019-03-06 16:59:36 +0700

    Create task_row.dart

---
+++ flutter_ui/lib/task_row.dart
@@ -0,0 +1,13 @@
+import 'package:filter_menu/task.dart';
+import 'package:flutter/material.dart';
+
+class TaskRow extends StatefulWidget {
+  
+}
+
+class _TaskRowState extends State<TaskRow> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
